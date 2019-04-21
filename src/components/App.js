b8caa3709439d10f6829add8bde4010a36c22726
Gahee <gahee0416@gmail.com>
2019-04-21 13:32:28 +0900

    return main page

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom'
-// import { Home } from '../pages';
-import Recent from '../pages/Recent';
+import { Home } from '../pages';
 // import Popular from 'pages/Popular';
 // import Bookmark from 'pages/Bookmark';
 
 const App = () => (
     <Switch>
-        <Route exact path="/" component={Recent}/>
-        <Route path="/:mode(recent|popular|bookmark)" component={Recent} />
+        <Route exact path="/" component={Home}/>
+        <Route path="/:mode(recent|popular|bookmark)" component={Home} />
     </Switch>
 )
 
