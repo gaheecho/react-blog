ede5dfe0f879bed1d5b214912db1ab963c48c593
Gahee <gahee0416@gmail.com>
2019-04-21 13:18:11 +0900

    replace main page to recent page

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom'
-import { Home } from '../pages';
+// import { Home } from '../pages';
+import Recent from '../pages/Recent';
 // import Popular from 'pages/Popular';
 // import Bookmark from 'pages/Bookmark';
 
 const App = () => (
     <Switch>
-        <Route exact path="/" component={Home}/>
-        <Route path="/:mode(recent|popular|bookmark)" component={Home} />
+        <Route exact path="/" component={Recent}/>
+        <Route path="/:mode(recent|popular|bookmark)" component={Recent} />
     </Switch>
 )
 
