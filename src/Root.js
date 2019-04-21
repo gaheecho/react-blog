8c9edd5af9b8761f2723a785a115227e606dd83c
Gahee <gahee0416@gmail.com>
2019-04-21 13:57:01 +0900

    replace BrowserRouter to HashRouter in react-router-dom

--- src/Root.js
+++ src/Root.js
@@ -1,15 +1,15 @@
 // @flow
 import React from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './components/App';
 import store from './store';
 
 const Root = () => (
     <Provider store={store} basename={process.env.PUBLIC_URL}>
-        <BrowserRouter>
+        <HashRouter>
             <App/>
-        </BrowserRouter>
+        </HashRouter>
     </Provider>
 );
 
